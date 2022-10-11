343a39c26f51a83a6d2d12c474d47dd8de89bfb1
Adolfo Herrera <valherdolf@gmail.com>
2022-10-11 19:42:04 -0400

    Add basic file structure for components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1 className="header__title">
+      Rick and Morty
+    </h1>
+    <p className="header__subtitle">Memory Card</p>
+  </header>
+);
+
+export default Header;
