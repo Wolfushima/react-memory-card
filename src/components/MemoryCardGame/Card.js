72946fe6617ffac55fe2f28d83af7421a043a5b0
Adolfo Herrera <valherdolf@gmail.com>
2022-10-14 21:09:30 -0400

    Add role, tabIndex for keyboard accessibility

--- src/components/MemoryCardGame/Card.js
+++ src/components/MemoryCardGame/Card.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Card = ({ src, alt, count }) => (
-  <div className={`card ${count}`}>
+const Card = ({
+  src, alt, count, handleClickOnCharacter, handleKeyDownOnCharacter,
+}) => (
+  <div
+    className={`card ${count}`}
+    onKeyDown={handleKeyDownOnCharacter}
+    onClick={handleClickOnCharacter}
+    role="button"
+    tabIndex={0}
+  >
     <img className="character" src={src} alt={alt} />
   </div>
 );
