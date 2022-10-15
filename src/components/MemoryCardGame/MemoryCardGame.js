fd96f3445ebb627bb1171511e9beccbb54b28bdc
Adolfo Herrera <valherdolf@gmail.com>
2022-10-15 05:08:44 -0400

    Add game logic

--- src/components/MemoryCardGame/MemoryCardGame.js
+++ src/components/MemoryCardGame/MemoryCardGame.js
@@ -1,18 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import OverviewMemoryCard from './OverviewMemoryCard';
 import fetchCharacters from './utils/fetchCharacters';
+import { handleCharacterClick, handleCharacterKeyDown } from './utils/handleClickAndKeyDownEvents';
+import ScoreBoard from './ScoreBoard';
 
 const MemoryCardGame = () => {
   const [data, setData] = useState([]);
+  const [lvl, setLevel] = useState('');
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(false);
 
+  const handleKeyDownOnCharacter = (e) => {
+    handleCharacterKeyDown(e, data, score, setScore, setData, setIsGameOver);
+  };
+
+  const handleClickOnCharacter = (e) => {
+    handleCharacterClick(e, data, score, setScore, setData, setIsGameOver);
+  };
+
+  /* Iinitialize game */
   useEffect(() => {
-    fetchCharacters(setData, 5);
+    fetchCharacters(setData, 2);
   }, []);
 
+  /* Level */
+  useEffect(() => {
+    if (lvl === 'EASY') { fetchCharacters(setData, 5); }
+    if (lvl === 'MEDIUM') { fetchCharacters(setData, 10); }
+    if (lvl === 'HARD') { fetchCharacters(setData, 15); }
+    if (lvl === 'EXTRAHARD') { fetchCharacters(setData, 20); }
+  }, [lvl]);
+
+  /* Game over */
+  useEffect(() => {
+    if (isGameOver) {
+      console.log('GAME OVER!');
+      fetchCharacters(setData, 2);
+      setScore(0);
+      setIsGameOver(false);
+    }
+  }, [isGameOver]);
+
+  /* Score */
+  useEffect(() => {
+    if (score === 2) { fetchCharacters(setData, 3); }
+    if (score === 5) { setLevel('EASY'); }
+    if (score === 10) { setLevel('MEDIUM'); }
+    if (score === 15) { setLevel('HARD'); }
+    if (score === 20) { setLevel('EXTRAHARD'); }
+    if (score > highScore) { setHighScore(score); }
+  }, [score]);
+
   return (
-    <OverviewMemoryCard
-      data={data}
-    />
+    <>
+      <ScoreBoard
+        score={score}
+        highScore={highScore}
+      />
+      <OverviewMemoryCard
+        data={data}
+        handleClickOnCharacter={handleClickOnCharacter}
+        handleKeyDownOnCharacter={handleKeyDownOnCharacter}
+      />
+    </>
   );
 };
 
--- src/components/MemoryCardGame/utils/handleClickAndKeyDownEvents.js
+++ src/components/MemoryCardGame/utils/handleClickAndKeyDownEvents.js
@@ -1,35 +1,43 @@
-const handleCharacterClick = (e, data, score, setScore, setData) => {
+const handleCharacterClick = (e, data, score, setScore, setData, setIsGameOver) => {
   const characterName = e.target.alt;
   const character = data.find((obj) => obj.name === characterName);
 
-  if (character.clicked === true) { return; }
+  if (character.clicked === true) {
+    setIsGameOver(true);
+    return;
+  }
 
   if (character.clicked === false) {
-    setData(data.map((obj) => {
-      if (obj.name === characterName) {
-        return { ...obj, clicked: true };
-      }
+    const nextData = data.map((obj) => {
+      if (obj.name === characterName) { return { ...obj, clicked: true }; }
       return obj;
-    }));
+    });
+    const shuffledData = [...nextData];
+    shuffledData.sort(() => Math.random() - 0.5);
+    setData(shuffledData);
   }
 
   setScore(score + 1);
 };
 
-const handleCharacterKeyDown = (e, data, score, setScore, setData) => {
+const handleCharacterKeyDown = (e, data, score, setScore, setData, setIsGameOver) => {
   if (e.key === 'Enter' || e.keyCode === 13) {
     const characterName = e.currentTarget.querySelector('img').alt;
     const character = data.find((obj) => obj.name === characterName);
 
-    if (character.clicked === true) { return; }
+    if (character.clicked === true) {
+      setIsGameOver(true);
+      return;
+    }
 
     if (character.clicked === false) {
-      setData(data.map((obj) => {
-        if (obj.name === characterName) {
-          return { ...obj, clicked: true };
-        }
+      const nextData = data.map((obj) => {
+        if (obj.name === characterName) { return { ...obj, clicked: true }; }
         return obj;
-      }));
+      });
+      const shuffledData = [...nextData];
+      shuffledData.sort(() => Math.random() - 0.5);
+      setData(shuffledData);
     }
 
     setScore(score + 1);
