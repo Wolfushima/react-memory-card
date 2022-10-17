9e9bb26db38b527701b94a798090bf106aff879a
Adolfo Herrera <valherdolf@gmail.com>
2022-10-17 19:09:25 -0400

    Add span to break line every 5 elements

--- src/components/MemoryCardGame/CardContainer.js
+++ src/components/MemoryCardGame/CardContainer.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import uniqid from 'uniqid';
 import Card from './Card';
 
 const CardContainer = ({ data, handleClickOnCharacter, handleKeyDownOnCharacter }) => (
   <div className="card-container">
     {
-    data.map((item, i) => (
-      <Card
-        src={item.url}
-        alt={item.name}
-        count={`card${i + 1}`}
-        key={uniqid()}
-        handleClickOnCharacter={handleClickOnCharacter}
-        handleKeyDownOnCharacter={handleKeyDownOnCharacter}
-      />
-    ))
-  }
+      data.map((item, i) => {
+        if (i === 5 || i === 10 || i === 15) {
+          return (
+            <Fragment key={uniqid()}>
+              <span className="break-line" />
+              <Card
+                src={item.url}
+                alt={item.name}
+                count={`card${i + 1}`}
+                key={uniqid()}
+              />
+            </Fragment>
+          );
+        }
+        return (
+          <Card
+            src={item.url}
+            alt={item.name}
+            count={`card${i + 1}`}
+            key={uniqid()}
+            handleClickOnCharacter={handleClickOnCharacter}
+            handleKeyDownOnCharacter={handleKeyDownOnCharacter}
+          />
+        );
+      })
+   }
   </div>
 );
 
